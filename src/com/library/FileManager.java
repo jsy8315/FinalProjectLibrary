80b6eee80328f207d1ca124d5c0b94ca9094c6d8
Suyoung Jung <110025650+jsy8315@users.noreply.github.com>
2023-03-26 14:31:48 +0900

    File클래스, FileManager클래스 추가

---
+++ src/com/library/FileManager.java
@@ -0,0 +1,25 @@
+package com.library;
+
+public class FileManager implements Manager {
+
+	@Override
+	public void add() { // csv 파일 만들기
+		
+	}
+
+	@Override
+	public void search() { //csv 파일에서 조회하기
+		
+	}
+
+	@Override
+	public void update() { //csv 파일 수정하기
+		
+	}
+
+	@Override
+	public void delete() { // csv 파일 삭제하기
+		
+	}
+
+}
--- src/com/library/MemberManager.java
+++ src/com/library/MemberManager.java
@@ -1,29 +1,45 @@
 package com.library;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // 회원 관리
 public class MemberManager implements Manager {
 
 	@Override
 	public void add() {
-		// TODO Auto-generated method stub
+		// 회원 가입
+		ArrayList<Object> memberlist = new ArrayList<>(); //정보를 어레이 리스트 타입으로 받을거임
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("회원 가입을 시작합니다.");
+		
 		
 	}
 
 	@Override
 	public void search() {
-		// TODO Auto-generated method stub
+		// 회원 조회
+		Scanner sc = new Scanner(System.in);
+		System.out.println("회원 조회를 시작합니다.");
+		System.out.println("이름을 입력하세요 : ");
+		String inputName = sc.next(); // 이름 입력
+		System.out.println("전화번호를 \"-\" 없이 입력하세요 : ");
+		String inputPhonenumber = sc.next(); // 전화번호 입력
+		
 		
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		// 회원 정보 수정
+		System.out.println("회원 정보 수정을 시작합니다.");
 	}
 
 	@Override
 	public void delete() {
-		// TODO Auto-generated method stub
-		
+		// 회원 정보 삭제
+		System.out.println("회원 정보 삭제를 시작합니다. ");
 	}
 
 }
